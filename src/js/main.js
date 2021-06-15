f8a1a4bfd6ff00c4e26bbcf08b2526f9ff9fcb3e
gouz <sylvain.gougouzian@gmail.com>
2021-06-16 00:12:25 +0200

    [+] parcel v2

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,6 @@
+import "../globals.css";
+
 import "nes.css/css/nes.min.css";
 import "@fontsource/press-start-2p";
+
+import "../less/main.less";
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.pug", "./src/**/*.js"],
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
+//"@fontsource/press-start-2p": "^4.2.1",
+//"nes.css": "^2.3.0"
